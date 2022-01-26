33892ba47f04bd0b351673cda9140a0cacbcd7ad
jrraymundo <jerome_raymundo@outlook.com>
2022-01-26 14:50:38 +0800

    Initial setup of enzyme and shallow render

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+/* eslint-disable testing-library/no-debugging-utils */
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug()) // Useful method for printing the html representation of the wrapper
 });
